7de6bff3d43b2d661b40856298bdf0c07425191e
rfluan <luanramosferr@outlook.com>
2022-07-20 19:04:56 -0300

    versão 3.4 (eu acho)

--- assets/js/mobile-navbar.js
+++ assets/js/mobile-navbar.js
@@ -7,4 +7,14 @@ function menuShow() {
       menuMobile.classList.add('open');
       document.querySelector('.icon').src = "assets/img/close_white_36dp.svg";
   }
+
+  function animateLinks() {
+    this.navLinks.forEach((link, index) => {
+      link.style.animation
+        ? (link.style.animation = "")
+        : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+            index / 7 + 0.3
+          }s`);
+    });
+  };
 }
