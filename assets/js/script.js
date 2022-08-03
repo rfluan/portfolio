5b9b3b82aeaa4ad109283d27a61985646dab9bf3
rfluan <luanramosferr@outlook.com>
2022-08-03 19:37:34 -0300

    js swiper

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
