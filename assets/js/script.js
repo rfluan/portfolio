cc2b55f27219ed57a7e5e517ac74db4603adaf9d
rfluan <luanramosferr@outlook.com>
2022-08-06 17:18:33 -0300

    btn top

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,3 +27,25 @@ var swiper = new Swiper(".slide-content", {
         },
     },
   });
+
+  /* BTN TOP */
+
+  const btn = document.getElementById('btnTop')
+
+  btn.addEventListener("click", function(){
+    window.scrollTo(0,0)
+  })
+
+  document.addEventListener('scroll',ocultar)
+
+  function ocultar(){
+    if(window.scrollY > 100){
+      btn.style.display = "flex"
+    } else {
+      btn.style.display = "none"
+    }
+  }
+
+  ocultar()
+
+  /* ---------------------//--------------------- */
